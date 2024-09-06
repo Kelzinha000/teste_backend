84acb91229d71bb73ab0ef8ed46dae748092b29c
Raquel <raquelbulhoes212@gmail.com>
2024-09-06 12:32:36 -0300

    atualizações 06/09

---
+++ test/atividade/funcao.js
@@ -0,0 +1,10 @@
+export function numerosPar(numeros){
+    return numeros.filter(numero=>numero%2 === 0)
+  
+}
+
+//  export function ordemAlfabetica(){
+    
+//   const ordem = lista.sort()
+//   return ordem
+//  }
---
+++ test/atividade/test.js
@@ -0,0 +1,12 @@
+import { numerosPar } from "./funcao.js";
+import { ordemAlfabetica } from "./funcao.js";
+
+test("Verifica número par", () => {
+  const resultado = [2, 4, 6, 8, 10];
+  expect(numerosPar([1, 2, 3,4,5,6,7,8,9,10])).toEqual(resultado);
+});
+
+// test("Ordem Alfabética", ()=>{
+//     const listaEsperada = ["Maria", "Raquel", "Deyse"]
+//     expect(ordemAlfabetica(["Deyse", "Maria", "Raquel"])).toEqual(listaEsperada)
+// })
